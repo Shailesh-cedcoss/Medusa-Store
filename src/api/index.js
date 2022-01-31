514a7ef47345c7215eb879bbb8f0f368810c23e1
Shailesh Kumar Dubey <shaileshkumardubey@makewebbetter.com>
2022-01-31 19:45:20 +0530

    requests-updated

--- src/api/index.js
+++ src/api/index.js
@@ -3,11 +3,45 @@ import bodyParser from "body-parser";
 
 export default () => {
   const app = Router();
-  app.get("/welcome", bodyParser.json(), async (req, res) => {
+  app.get("/welcome-create", bodyParser.json(), async (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
     const draftOrderService = req.scope.resolve("draftOrderService");
-
+    const data = {
+      email: "idk@email.com",
+      customer_id: "cus_01FTQXNHFGAPJX2FYX91VM6PBH",
+      region_id: "reg_01FTQVGGTD3109XBF8JQCHB1YH",
+      shipping_methods: [
+        {
+          option_id: "so_01FTQVGGVC9E0PCX39P6GFER6M",
+        },
+      ],
+      items: [
+        {
+          title: "XL / Black",
+          variant_id: req.query.id,
+          quantity: 10,
+        },
+      ],
+    };
+    let draftOrder = await draftOrderService.create(data);
+    res.json({
+      draft: draftOrder,
+    });
+  });
+  app.get("/welcome-list", bodyParser.json(), async (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    const draftOrderService = req.scope.resolve("draftOrderService");
     const selector = {};
-
     const listConfig = {
       select: [
         "id",
@@ -26,12 +60,10 @@ export default () => {
       take: 20,
       order: { created_at: "DESC" },
     };
-
     const [draftOrders, count] = await draftOrderService.listAndCount(
       selector,
       listConfig
     );
-
     res.json({
       draft_orders: draftOrders,
       count,
--- src/services/welcome.js
+++ src/services/welcome.js
@@ -79,184 +79,184 @@ class DraftOrderService extends BaseService {
     return [draftOrders, count];
   }
 
-  //   /**
-  //    * Lists draft orders
-  //    * @param {Object} selector - query object for find
-  //    * @param {Object} config - configurable attributes for find
-  //    * @return {Promise<DraftOrder>} list of draft orders
-  //    */
-  //   async list(
-  //     selector,
-  //     config = { skip: 0, take: 50, order: { created_at: "DESC" } }
-  //   ) {
-  //     const draftOrderRepo = this.manager_.getCustomRepository(
-  //       this.draftOrderRepository_
-  //     );
+  /**
+   * Lists draft orders
+   * @param {Object} selector - query object for find
+   * @param {Object} config - configurable attributes for find
+   * @return {Promise<DraftOrder>} list of draft orders
+   */
+  async list(
+    selector,
+    config = { skip: 0, take: 50, order: { created_at: "DESC" } }
+  ) {
+    const draftOrderRepo = this.manager_.getCustomRepository(
+      this.draftOrderRepository_
+    );
 
-  //     const query = this.buildQuery_(selector, config);
+    const query = this.buildQuery_(selector, config);
 
-  //     return draftOrderRepo.find(query);
-  //   }
+    return draftOrderRepo.find(query);
+  }
 
-  //   /**
-  //    * Creates a draft order.
-  //    * @param {object} data - data to create draft order from
-  //    * @return {Promise<DraftOrder>} the created draft order
-  //    */
-  //   async create(data) {
-  //     return this.atomicPhase_(async (manager) => {
-  //       const draftOrderRepo = manager.getCustomRepository(
-  //         this.draftOrderRepository_
-  //       );
+  /**
+   * Creates a draft order.
+   * @param {object} data - data to create draft order from
+   * @return {Promise<DraftOrder>} the created draft order
+   */
+  async create(data) {
+    return this.atomicPhase_(async (manager) => {
+      const draftOrderRepo = manager.getCustomRepository(
+        this.draftOrderRepository_
+      );
 
-  //       if (!data.region_id) {
-  //         throw new MedusaError(
-  //           MedusaError.Types.INVALID_DATA,
-  //           `region_id is required to create a draft order`
-  //         );
-  //       }
+      if (!data.region_id) {
+        throw new MedusaError(
+          MedusaError.Types.INVALID_DATA,
+          `region_id is required to create a draft order`
+        );
+      }
 
-  //       if (!data.items || !data.items.length) {
-  //         throw new MedusaError(
-  //           MedusaError.Types.INVALID_DATA,
-  //           `Items are required to create a draft order`
-  //         );
-  //       }
+      if (!data.items || !data.items.length) {
+        throw new MedusaError(
+          MedusaError.Types.INVALID_DATA,
+          `Items are required to create a draft order`
+        );
+      }
 
-  //       const {
-  //         shipping_methods,
-  //         discounts,
-  //         no_notification_order,
-  //         items,
-  //         ...rest
-  //       } = data;
+      const {
+        shipping_methods,
+        discounts,
+        no_notification_order,
+        items,
+        ...rest
+      } = data;
 
-  //       if (discounts) {
-  //         for (const { code } of discounts) {
-  //           rest.discounts = [];
-  //           await this.cartService_
-  //             .withTransaction(manager)
-  //             .applyDiscount(rest, code);
-  //         }
-  //       }
+      if (discounts) {
+        for (const { code } of discounts) {
+          rest.discounts = [];
+          await this.cartService_
+            .withTransaction(manager)
+            .applyDiscount(rest, code);
+        }
+      }
 
-  //       const createdCart = await this.cartService_
-  //         .withTransaction(manager)
-  //         .create({ type: "draft_order", ...rest });
+      const createdCart = await this.cartService_
+        .withTransaction(manager)
+        .create({ type: "draft_order", ...rest });
 
-  //       const draftOrder = draftOrderRepo.create({
-  //         cart_id: createdCart.id,
-  //         no_notification_order,
-  //       });
-  //       const result = await draftOrderRepo.save(draftOrder);
+      const draftOrder = draftOrderRepo.create({
+        cart_id: createdCart.id,
+        no_notification_order,
+      });
+      const result = await draftOrderRepo.save(draftOrder);
 
-  //       await this.eventBus_
-  //         .withTransaction(manager)
-  //         .emit(DraftOrderService.Events.CREATED, {
-  //           id: result.id,
-  //         });
+      await this.eventBus_
+        .withTransaction(manager)
+        .emit(DraftOrderService.Events.CREATED, {
+          id: result.id,
+        });
 
-  //       for (const item of items) {
-  //         if (item.variant_id) {
-  //           const line = await this.lineItemService_
-  //             .withTransaction(manager)
-  //             .generate(item.variant_id, data.region_id, item.quantity, {
-  //               metadata: item?.metadata || {},
-  //               unit_price: item.unit_price,
-  //             });
+      for (const item of items) {
+        if (item.variant_id) {
+          const line = await this.lineItemService_
+            .withTransaction(manager)
+            .generate(item.variant_id, data.region_id, item.quantity, {
+              metadata: item?.metadata || {},
+              unit_price: item.unit_price,
+            });
 
-  //           await this.lineItemService_.withTransaction(manager).create({
-  //             cart_id: createdCart.id,
-  //             ...line,
-  //           });
-  //         } else {
-  //           let price;
-  //           if (typeof item.unit_price === `undefined` || item.unit_price < 0) {
-  //             price = 0;
-  //           } else {
-  //             price = item.unit_price;
-  //           }
+          await this.lineItemService_.withTransaction(manager).create({
+            cart_id: createdCart.id,
+            ...line,
+          });
+        } else {
+          let price;
+          if (typeof item.unit_price === `undefined` || item.unit_price < 0) {
+            price = 0;
+          } else {
+            price = item.unit_price;
+          }
 
-  //           // custom line items can be added to a draft order
-  //           await this.lineItemService_.withTransaction(manager).create({
-  //             cart_id: createdCart.id,
-  //             has_shipping: true,
-  //             title: item.title || "Custom item",
-  //             allow_discounts: false,
-  //             unit_price: price,
-  //             quantity: item.quantity,
-  //           });
-  //         }
-  //       }
+          // custom line items can be added to a draft order
+          await this.lineItemService_.withTransaction(manager).create({
+            cart_id: createdCart.id,
+            has_shipping: true,
+            title: item.title || "Custom item",
+            allow_discounts: false,
+            unit_price: price,
+            quantity: item.quantity,
+          });
+        }
+      }
 
-  //       for (const method of shipping_methods) {
-  //         await this.cartService_
-  //           .withTransaction(manager)
-  //           .addShippingMethod(createdCart.id, method.option_id, method.data);
-  //       }
+      for (const method of shipping_methods) {
+        await this.cartService_
+          .withTransaction(manager)
+          .addShippingMethod(createdCart.id, method.option_id, method.data);
+      }
 
-  //       return result;
-  //     });
-  //   }
+      return result;
+    });
+  }
 
-  //   /**
-  //    * Registers a draft order as completed, when an order has been completed.
-  //    * @param {string} doId - id of draft order to complete
-  //    * @param {string} orderId - id of order completed from draft order cart
-  //    * @return {Promise} the created order
-  //    */
-  //   async registerCartCompletion(doId, orderId) {
-  //     return this.atomicPhase_(async (manager) => {
-  //       const draftOrderRepo = manager.getCustomRepository(
-  //         this.draftOrderRepository_
-  //       );
-  //       const draftOrder = await this.retrieve(doId);
+  /**
+   * Registers a draft order as completed, when an order has been completed.
+   * @param {string} doId - id of draft order to complete
+   * @param {string} orderId - id of order completed from draft order cart
+   * @return {Promise} the created order
+   */
+  async registerCartCompletion(doId, orderId) {
+    return this.atomicPhase_(async (manager) => {
+      const draftOrderRepo = manager.getCustomRepository(
+        this.draftOrderRepository_
+      );
+      const draftOrder = await this.retrieve(doId);
 
-  //       draftOrder.status = "completed";
-  //       draftOrder.completed_at = new Date();
-  //       draftOrder.order_id = orderId;
+      draftOrder.status = "completed";
+      draftOrder.completed_at = new Date();
+      draftOrder.order_id = orderId;
 
-  //       await draftOrderRepo.save(draftOrder);
-  //     });
-  //   }
+      await draftOrderRepo.save(draftOrder);
+    });
+  }
 
-  //   /**
-  //    * Updates a draft order with the given data
-  //    * @param {String} doId - id of the draft order
-  //    * @param {DraftOrder} data - values to update the order with
-  //    * @return {Promise<DraftOrder>} the updated draft order
-  //    */
-  //   async update(doId, data) {
-  //     return this.atomicPhase_(async (manager) => {
-  //       const doRepo = manager.getCustomRepository(this.draftOrderRepository_);
-  //       const draftOrder = await this.retrieve(doId);
-  //       let touched = false;
+  /**
+   * Updates a draft order with the given data
+   * @param {String} doId - id of the draft order
+   * @param {DraftOrder} data - values to update the order with
+   * @return {Promise<DraftOrder>} the updated draft order
+   */
+  async update(doId, data) {
+    return this.atomicPhase_(async (manager) => {
+      const doRepo = manager.getCustomRepository(this.draftOrderRepository_);
+      const draftOrder = await this.retrieve(doId);
+      let touched = false;
 
-  //       if (draftOrder.status === "completed") {
-  //         throw new MedusaError(
-  //           MedusaError.Types.NOT_ALLOWED,
-  //           "Can't update a draft order which is complete"
-  //         );
-  //       }
+      if (draftOrder.status === "completed") {
+        throw new MedusaError(
+          MedusaError.Types.NOT_ALLOWED,
+          "Can't update a draft order which is complete"
+        );
+      }
 
-  //       if (data.no_notification_order !== undefined) {
-  //         touched = true;
-  //         draftOrder.no_notification_order = data.no_notification_order;
-  //       }
+      if (data.no_notification_order !== undefined) {
+        touched = true;
+        draftOrder.no_notification_order = data.no_notification_order;
+      }
 
-  //       if (touched) {
-  //         doRepo.save(draftOrder);
+      if (touched) {
+        doRepo.save(draftOrder);
 
-  //         await this.eventBus_
-  //           .withTransaction(manager)
-  //           .emit(DraftOrderService.Events.UPDATED, {
-  //             id: draftOrder.id,
-  //           });
-  //       }
+        await this.eventBus_
+          .withTransaction(manager)
+          .emit(DraftOrderService.Events.UPDATED, {
+            id: draftOrder.id,
+          });
+      }
 
-  //       return draftOrder;
-  //     });
-  //   }
+      return draftOrder;
+    });
+  }
 }
 
 export default DraftOrderService;
