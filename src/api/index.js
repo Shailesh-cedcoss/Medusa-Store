a28b92c06f531516b399dabe9af2096b3490e543
sudhanshukrdev <sudhanshukumar@makewebbetter.com>
2022-02-09 16:02:55 +0530

    install packages

--- src/api/index.js
+++ src/api/index.js
@@ -1,40 +1,39 @@
-import { Router } from "express";
-import bodyParser from "body-parser";
-import { MedusaError } from "medusa-core-utils";
-import { allowCors } from "./functions/allow-cors";
-import { addToCart } from "./functions/add-to-cart";
-import { getOrderId } from "./functions/create-order-payment";
-import { changeOrderStatus } from "./functions/draft-order-status";
-import Razorpay from "razorpay";
+import { Router } from 'express';
+import bodyParser from 'body-parser';
+import Razorpay from 'razorpay';
+import { allowCors } from './functions/allow-cors';
+import { addToCart } from './functions/add-to-cart';
+import { getOrderId } from './functions/create-order-payment';
+import { changeOrderStatus } from './functions/draft-order-status';
 
 export default () => {
   const app = Router();
-  app.get("/add-to-cart", bodyParser.json(), async (req, res) => {
+  app.get('/add-to-cart', bodyParser.json(), async (req, res) => {
     allowCors(res);
     res.json(await addToCart(req));
   });
 
-  app.get("/welcome-list", bodyParser.json(), async (req, res) => {
+  app.get('/welcome-list', bodyParser.json(), async (req, res) => {
     allowCors(res);
-    const draftOrderService = req.scope.resolve("draftOrderService");
+    const draftOrderService = req.scope.resolve('draftOrderService');
     const selector = {};
     const listConfig = {
       select: [
-        "id",
-        "status",
-        "display_id",
-        "cart_id",
-        "order_id",
-        "canceled_at",
-        "created_at",
-        "updated_at",
-        "metadata",
-        "no_notification_order",
+        'id',
+        'status',
+        'display_id',
+        'cart_id',
+        'order_id',
+        'canceled_at',
+        'created_at',
+        'updated_at',
+        'metadata',
+        'no_notification_order',
       ],
-      relations: ["order", "cart"],
+      relations: ['order', 'cart'],
       skip: 0,
       take: 20,
-      order: { created_at: "DESC" },
+      order: { created_at: 'DESC' },
     };
     const [draftOrders, count] = await draftOrderService.listAndCount(
       selector,
@@ -48,17 +47,17 @@ export default () => {
     });
   });
 
-  app.get("/create-payment-order", bodyParser.json(), async (req, res) => {
+  app.get('/create-payment-order', bodyParser.json(), async (req, res) => {
     allowCors(res);
     const instance = new Razorpay({
-      key_id: "rzp_test_dIIyGFY7lcgcVs",
-      key_secret: "5UBJil2uuLDFoLHkF9Th8GzH",
+      key_id: 'rzp_test_dIIyGFY7lcgcVs',
+      key_secret: '5UBJil2uuLDFoLHkF9Th8GzH',
     });
 
     res.json(await getOrderId(instance, req));
   });
 
-  app.get("/change-draft-order-status", bodyParser.json(), async (req, res) => {
+  app.get('/change-draft-order-status', bodyParser.json(), async (req, res) => {
     allowCors(res);
     res.json(await changeOrderStatus(req));
   });
